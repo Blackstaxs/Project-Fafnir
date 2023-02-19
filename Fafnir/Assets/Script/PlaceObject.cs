319ab900a2772dca9e345c1389b1205114888f86
Blackstaxs <diegoruizx2@hotmail.com>
2023-02-19 13:27:40 -0800

    Placing objects on planes detected

---
+++ Fafnir/Assets/Script/PlaceObject.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using EnhancedTouch = UnityEngine.InputSystem.EnhancedTouch;
+
+[RequireComponent(typeof(ARRaycastManager), typeof(ARPlaneManager))]
+public class PlaceObject : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject preFab;
+
+    private ARRaycastManager aRRaycastManager;
+    private ARPlaneManager aRPlaneManager;
+    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    private void Awake()
+    {
+        aRRaycastManager = GetComponent<ARRaycastManager>();
+        aRPlaneManager= GetComponent<ARPlaneManager>();
+    }
+
+    private void OnEnable()
+    {
+        EnhancedTouch.EnhancedTouchSupport.Enable();
+        EnhancedTouch.Touch.onFingerDown += FingerDown;
+    }
+
+    private void OnDisable()
+    {
+        EnhancedTouch.EnhancedTouchSupport.Disable();
+        EnhancedTouch.Touch.onFingerDown -= FingerDown;
+    }
+
+    private void FingerDown(EnhancedTouch.Finger finger)
+    {
+        if (finger.index != 0) return;
+
+        if (aRRaycastManager.Raycast(finger.currentTouch.screenPosition, hits,TrackableType.PlaneWithinPolygon)) 
+        { 
+            foreach(ARRaycastHit hit in hits)
+            {
+                Pose pose = hit.pose;
+                GameObject obj = Instantiate(preFab, pose.position, pose.rotation);
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
